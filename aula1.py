22d7d422b021e4e2f9d976ae0649416adaf5f819
Lailson Lira <lailsonlira10@gmail.com>
2026-06-03 14:08:10 -0300

    inicio

---
+++ aula1.py
@@ -0,0 +1,20 @@
+"""
+DocString
+E escrever o que 
+quiser
+
+
+asdafkem
+"""
+
+
+
+
+
+
+'''Usar para escrever suas notas'''
+
+# Permite escrever um comentário
+print(123) #Na frente
+# Abaixo
+print(456)
