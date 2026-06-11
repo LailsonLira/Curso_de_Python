4284400e41c4ca0d442804850851e224b93b42cd
Lailson Lira <lailsonlira10@gmail.com>
2026-06-11 11:32:45 -0300

    1

---
+++ aula8.py
@@ -0,0 +1,14 @@
+
+nome = 'Lailson'
+sobrenome = 'Lira'
+idade = 30
+ano_de_nascimento = 1992
+maior_de_idade = 18
+altura_metros = 165
+
+print('Nome', nome)
+print('Sobrenome', sobrenome)
+print('Idade', idade)
+print('Ano de nascimento', ano_de_nascimento)
+print('E maior de Idade?', maior_de_idade)
+print('Altura em metros:', altura_metros)
